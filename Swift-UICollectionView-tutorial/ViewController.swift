29911b29d6ba074b5ac672c91ef4dd9f2e2e8cbd
Jaf Crisologo <rtql8d@gmail.com>
2020-02-01 18:05:36 -0700

    First commit

---
+++ Swift-UICollectionView-tutorial/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Swift-UICollectionView-tutorial
+//
+//  Created by Jaf Crisologo on 2020-02-01.
+//  Copyright © 2020 Modium Design. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
